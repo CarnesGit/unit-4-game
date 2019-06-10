30a8285f86bab9acddad94662a3dc350a3270f65
Rick <rick.carnes@gmail.com>
2019-06-10 09:02:24 -0400

    added ship object

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -7,6 +7,40 @@ $(document).ready(function() {
     var wins = 0;
     var losses = 0;
 
+    // Object for ship Stats
+    var ship = {
+        "USS Enterprise": {
+            name: "USS Enterprise",
+            health: 120,
+            attack: 8,
+            enemyAttackBack: 15
+        },
+        "Borg Cube": {
+            name: "Borg Cube",
+            health: 100,
+            attack: 14,
+            enemyAttackBack: 5
+        },
+        "Defiant": {
+            name: "Defiant",
+            health: 150,
+            attack: 8,
+            enemyAttackBack: 20
+        },
+        "Klingon Bird of Prey": {
+            name: "Bird of Prey",
+            health: 180,
+            attack: 7,
+            enemyAttackBack: 25
+        },
+        "Romulan Warbird": {
+            name: "Warbird",
+            health: 180,
+            attack: 7,
+            enemyAttackBack: 25
+        }
+    };
+
     // Enemy List (Once a selection was made for the player ship the rest of the ships will be added to this array.)
     var enemies = [
 
@@ -14,12 +48,22 @@ $(document).ready(function() {
 
     //Variable for layout (Change the bootstrap classes to rearange layout, and hide elements if needed. These presets will be stored here.)
     var layOut = {
-        "enemySelectScreen": "",
-        "combatScreen": "",
-        "winScreen": "",
-        "loseScreen": "",
-        "winGameScreen": "",
-        "loseGameScreen": ""
+        "enemySelectScreen": {},
+        "combatScreen": {
+
+        },
+        "winScreen": {
+
+        },
+        "loseScreen": {
+
+        },
+        "winGameScreen": {
+
+        },
+        "loseGameScreen": {
+
+        }
     };
 
     //Function to select a Player Ship and move to the Enemy Selection Screen
@@ -28,27 +72,27 @@ $(document).ready(function() {
     };
 
     //Function to select an Enemy and move to the Combat Screen for each stage of the game
-    function $() {
+    function() {
 
     };
 
     //Function for Combat
-    function $() {
+    function() {
 
     };
 
     //Function to adjust score and move to Win or Lose Screen
-    function $() {
+    function() {
 
     };
 
     //Function for the Win and Lose Game Sceanarios
-    function $() {
+    function() {
 
     };
 
     //Reset Game Function
-    function $() {
+    function() {
 
     }
 
